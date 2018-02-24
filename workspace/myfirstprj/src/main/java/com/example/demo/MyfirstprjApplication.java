217b264f22e4fe0ea2c61215340f621198753b09
KUMAR Vivek <vivek.kumar2@soprasteria.com>
2018-02-24 14:16:03 +0530

    new project

---
+++ workspace/myfirstprj/src/main/java/com/example/demo/MyfirstprjApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprjApplication.class, args);
+	}
+}
